248106876aaabfa8bf3c0fe755bd51e26c96c944
beanmedley2 <98910301+beanmedley2@users.noreply.github.com>
2022-02-08 18:19:51 -0500

    added a small description and square roots

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 
+#just a small calculator that does all the standard eqautions at once
+
 first_number = input("First Number:")
 second_number = input("Second Number:")
 (multi_ply): input = "multiply"
 (sub_tract): input = "subtract"
 (div_ide): input = "divide"
 (add_ition): input = "addition"
+(square_root_1): input="1st number square root"
+(square_root_2): input="2nd number square root"
+(sumsqr1) = int(first_number) * int(first_number)
+square_root_1: print ("The square root of the first number is")
+print(sumsqr1)
+(sumsqr2) = int(second_number) * int(second_number)
+square_root_2: print ("The square root of the second number is")
+print(sumsqr2)
 (summ) = int(first_number) * int(second_number)
 multi_ply: print("The product is")
 print(summ)
