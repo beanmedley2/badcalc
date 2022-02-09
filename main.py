e7ebe3085ddeb8544f6ea50b37f0b4ce4a808e23
beanmedley2 <98910301+beanmedley2@users.noreply.github.com>
2022-02-09 08:44:36 -0500

    fixed square root issue using math library
    rather large bugfix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 #just a small calculator that does all the standard eqautions at once
 
+import math
+
 first_number = input("First Number:")
 second_number = input("Second Number:")
 (multi_ply): input = "multiply"
@@ -9,10 +11,10 @@ second_number = input("Second Number:")
 (add_ition): input = "addition"
 (square_root_1): input="1st number square root"
 (square_root_2): input="2nd number square root"
-(sumsqr1) = int(first_number) / int(first_number)
+(sumsqr1) = math.sqrt(int(first_number))
 square_root_1: print ("The square root of the first number is")
 print(sumsqr1)
-(sumsqr2) = int(second_number) / int(second_number)
+(sumsqr2) = math.sqrt(int(second_number))
 square_root_2: print ("The square root of the second number is")
 print(sumsqr2)
 (summ) = int(first_number) * int(second_number)
@@ -27,4 +29,6 @@ print(sumd)
 (suma) = int(first_number) + int(second_number)
 add_ition: print("The sum is")
 print(suma)
+
+
 
